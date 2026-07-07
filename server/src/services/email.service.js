2d153001533e719720a5109e557f09cc735b2da4
Prashant Singh <prashantsin2gh@gmail.com>
2026-07-08 01:56:52 +0530

    test noadmailer service with brevo

--- server/src/services/email.service.js
+++ server/src/services/email.service.js
@@ -1,26 +1,5 @@
 const nodemailer = require("nodemailer");
 
-const transporter = nodemailer.createTransport({
-  host: process.env.SMTP_HOST,
-  port: Number(process.env.SMTP_PORT),
-  secure: false, // Port 587 (STARTTLS)
-  auth: {
-    user: process.env.SMTP_USER,
-    pass: process.env.SMTP_PASS,
-  },
-  connectionTimeout: 10000,
-  greetingTimeout: 10000,
-  socketTimeout: 10000,
-});
-
-// Verify SMTP connection when server starts
-transporter.verify((error) => {
-  if (error) {
-    console.error("❌ SMTP Connection Error:", error);
-  } else {
-    console.log("✅ SMTP Server Ready");
-  }
-});
 
 const sendVerificationEmail = async (email, token) => {
   const verificationUrl = `${process.env.CLIENT_URL}/verify-email?token=${token}`;
